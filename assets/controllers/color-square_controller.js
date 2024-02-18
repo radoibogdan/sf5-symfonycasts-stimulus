127ce42ec15f556bc99a7fd358d917c78c5d4bc8
Bogdan Radoi <radoi.office@gmail.com>
2024-02-18 18:35:25 +0100

    Updates the <select> tag value with the color id from the selected button using targets

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -1,7 +1,7 @@
 import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ['colorSquare'];
+    static targets = ['colorSquare', 'select'];
 
     selectColor(event) {
         this.colorSquareTargets.forEach((element) => {
@@ -10,5 +10,11 @@ export default class extends Controller {
 
         /* event has 2 important properties event.currentTarget and even.target */
         event.currentTarget.classList.add('selected');
+
+        /*
+         * Updates the <select> tag value with the color id from the selected button using targets
+         * data-color-id in html becomes dataset.colorId in js
+        */
+        this.selectTarget.value = event.currentTarget.dataset.colorId
     }
 };
