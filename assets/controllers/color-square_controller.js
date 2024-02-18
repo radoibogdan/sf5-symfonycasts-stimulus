b69232bf53c5ccc89a3ce44ea1cf700e9003c600
Bogdan Radoi <radoi.office@gmail.com>
2024-02-18 18:08:32 +0100

    Add css to btn using event.currentTarget

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -1,7 +1,14 @@
 import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
+    static targets = ['colorSquare'];
+
     selectColor(event) {
-        console.log(event);
+        this.colorSquareTargets.forEach((element) => {
+            element.classList.remove('selected')
+        })
+
+        /* event has 2 important properties event.currentTarget and even.target */
+        event.currentTarget.classList.add('selected');
     }
 };
