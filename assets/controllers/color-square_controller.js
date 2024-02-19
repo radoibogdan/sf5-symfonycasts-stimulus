15cab0e5f49e2217a4ea54d091303b4163ec0b7f
Bogdan Radoi <radoi.office@gmail.com>
2024-02-19 23:42:23 +0100

    State

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -1,6 +1,7 @@
 import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
+    selectedColorId = null
     static targets = ['colorSquare', 'select'];
 
     connect() {
@@ -9,17 +10,42 @@ export default class extends Controller {
     }
 
     selectColor(event) {
+        /* event has 2 important properties event.currentTarget and even.target */
+        this.setSelectedColor(event.currentTarget.dataset.colorId);
+    }
+
+    setSelectedColor(clickedColorId) {
+        /* Clicked a color that is already selected*/
+        if (clickedColorId === this.selectedColorId) {
+            this.findSelectedColorSquare().classList.remove('selected');
+            this.selectedColorId = null;
+            this.selectTarget.value = '';
+
+            return;
+        }
+
+        /* Set selectedColor state */
+        this.selectedColorId = clickedColorId;
+
+        /* Remove all css */
         this.colorSquareTargets.forEach((element) => {
             element.classList.remove('selected')
         })
 
-        /* event has 2 important properties event.currentTarget and even.target */
-        event.currentTarget.classList.add('selected');
+        /* Add css */
+        this.findSelectedColorSquare().classList.add('selected');
 
         /*
          * Updates the <select> tag value with the color id from the selected button using targets
          * data-color-id in html becomes dataset.colorId in js
         */
-        this.selectTarget.value = event.currentTarget.dataset.colorId
+        this.selectTarget.value = this.selectedColorId;
+    }
+
+    /**
+     * @returns {Element|null}
+     */
+    findSelectedColorSquare() {
+        return this.colorSquareTargets.find((element) => element.dataset.colorId === this.selectedColorId);
     }
 };
