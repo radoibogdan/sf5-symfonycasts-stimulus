4bbae66951004be893a94e600bc01d503eeb1b25
Bogdan Radoi <radoi.office@gmail.com>
2024-03-06 23:35:11 +0100

    JS Async/ Dynamic import

--- assets/controllers/featured-product-react_controller.js
+++ assets/controllers/featured-product-react_controller.js
@@ -1,5 +1,5 @@
 import {Controller} from "@hotwired/stimulus";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import React from "react";
 import FeaturedProduct from "../components/FeaturedProduct";
 
@@ -9,9 +9,16 @@ export default class extends Controller {
     }
 
     connect() {
-        ReactDOM.render(
-            <FeaturedProduct product={this.productValue} />,
-            this.element
-        )
+        /**
+         * Async/ Dynamic import
+         * Allows webpack to isolate the ReactDOM code in its own file
+         * The code won't be dowloaaded until the import line is executed
+        */
+        import('react-dom').then((ReactDOM) => {
+            ReactDOM.default.render(
+                <FeaturedProduct product={this.productValue} />,
+                this.element
+            )
+        });
     }
 }
--- assets/controllers/made-with-love_controller.js
+++ assets/controllers/made-with-love_controller.js
@@ -1,13 +1,20 @@
 import {Controller} from "@hotwired/stimulus";
 import MadeWithLove from "../components/MadeWithLove";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import React from "react";
 
 export default class extends Controller {
   connect() {
-      ReactDOM.render(
-          <MadeWithLove />,
-          this.element
-      )
+      /**
+       * Async/ Dynamic import
+       * Allows webpack to isolate the ReactDOM code in its own file
+       * The code won't be dowloaaded until the import line is executed
+       */
+      import('react-dom').then((ReactDOM) => {
+          ReactDOM.default.render(
+              <MadeWithLove />,
+              this.element
+          )
+      })
   }
 }
