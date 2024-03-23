7bf75c17c3ee14c16d483796165cbfe34ebeab84
Bogdan Radoi <radoi.office@gmail.com>
2024-03-23 19:56:50 +0100

    3rd party controller (Autocomplete) - Lazy

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,6 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
-import { Autocomplete } from 'stimulus-autocomplete'
+/* This line should make LAZY the 3rd party library 'autocomplete', but does not work */
+import { Autocomplete } from '@symfony/stimulus-bridge/lazy-controller-loader?lazy=true&export=Autocomplete!stimulus-autocomplete';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
